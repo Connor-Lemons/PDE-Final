0478fd58fe6240a922b17cfd4e9b6f8779caafe8
SuccessCheck <161098324+SuccessCheck@users.noreply.github.com>
2025-05-09 21:35:29 -0600

    Fixed Radiator fin axes

--- Radiator_fin.m
+++ Radiator_fin.m
@@ -6,22 +6,9 @@ function [x_vals, y_vals, X, delta_x, delta_y] = Radiator_fin(x_iter, y_iter, TO
     X = zeros(x_iter, y_iter, 1);
     x_vals = linspace(0,L,x_iter);
     y_vals = linspace(0,H,y_iter);
-    
-    X(1:(x_iter+1)/2,1:(y_iter+1)/2,1) = 0;
-    X((x_iter+1)/2:end,1,1) = 1;
-    X(1,(y_iter+1)/2:end,1) = 1;
-    X(end,:,1) = 1;
-    X(:,end,1) = x_vals;
-    
+
     delta_x = x_vals(2) - x_vals(1);
     delta_y = y_vals(2) - y_vals(1);
-    f = @(x, y) 1 + 0.2 * double(x == 1 & y == 3);
-    f_mat = zeros(x_iter,y_iter);
-    for i = 1:x_iter
-        for j = 1:y_iter
-            f_mat(i,j) = f(x_vals(i), y_vals(j));
-        end
-    end
 
     for i = 2:max_iter
 
@@ -29,13 +16,14 @@ function [x_vals, y_vals, X, delta_x, delta_y] = Radiator_fin(x_iter, y_iter, TO
         X(1,:,i) = 0;
         X(:,end,i) = beta*x_vals.*(L-x_vals);
         X(end,:,i) = 0;
-        X(2:x_iter-1,2:y_iter-1,i) = 0.25*(X(3:x_iter,2:y_iter-1,i-1) + X(1:x_iter-2,2:y_iter-1,i-1) + X(2:x_iter-1,3:y_iter,i-1) + X(2:x_iter-1,1:y_iter-2,i-1) - delta_x^2*f_mat(2:x_iter-1,2:y_iter-1));
+        X(2:x_iter-1,2:y_iter-1,i) = 0.25*(X(3:x_iter,2:y_iter-1,i-1) + X(1:x_iter-2,2:y_iter-1,i-1) + X(2:x_iter-1,3:y_iter,i-1) + X(2:x_iter-1,1:y_iter-2,i-1));
         if max(abs(X(:,:,i) - X(:,:,i-1)),[],"all") <= TOL
             break
         end
     end
    
-    decide = string(1);
+    str = "";
+    decide = input(str + "Press enter to exit. Type 1 for Finite Difference Solution", "s");
     switch decide
         case "1"
 
@@ -44,7 +32,7 @@ function [x_vals, y_vals, X, delta_x, delta_y] = Radiator_fin(x_iter, y_iter, TO
             
             figure;
             hold on
-            h = surf(X_grid, Y_grid, X(:,1:2:end,end), "FaceAlpha", 0.25, 'FaceColor', 'b');
+            h = surf(X_grid, Y_grid, X(:,1:2:end,end)', "FaceAlpha", 0.25, 'FaceColor', 'b');
             xlabel('x');
             ylabel('y');
             zlabel('Temperature');
