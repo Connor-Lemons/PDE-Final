940e96079dab9cefe9c9626922c91c5386113ac9
SuccessCheck <161098324+SuccessCheck@users.noreply.github.com>
2025-05-09 23:04:01 -0600

    Figured out MOL

---
+++ Radiator_fin_MOL.m
@@ -0,0 +1,51 @@
+function [y_vals, x_vals, X, delta_x, delta_y] = Radiator_fin_MOL(t_iter, x_iter)    
+    y_0 = 0;
+    y_f = pi/16;
+    y_vals = linspace(y_0, y_f, t_iter);
+    beta=10000;
+
+
+    L = pi/8;
+    X = zeros(t_iter, x_iter);
+    x_vals = linspace(0,L,x_iter);
+    X(end, :) = 0;
+    X(:,1) = 0;
+    X(:,end) = 0;
+    X(1,:)= beta*x_vals.*(L-x_vals);
+    
+    K = zeros(4, x_iter);
+    delta_y = y_vals(2) - y_vals(1);
+    delta_x = x_vals(2) - x_vals(1);
+
+    for i = 2 : t_iter
+        K(1,2:x_iter-1) = (X(i-1,3:x_iter) - 2*X(i-1,2:x_iter-1) + X(i-1,1:x_iter-2))/delta_x^2;
+        K(2,2:x_iter-1) = ((X(i-1,3:x_iter) + (delta_y/2)*K(1,3:x_iter)) - 2*(X(i-1,2:x_iter-1) + (delta_y/2)*K(1,2:x_iter-1)) + (X(i-1,1:x_iter-2) + (delta_y/2)*K(1,1:x_iter-2)))/delta_x^2;
+        K(3,2:x_iter-1) = ((X(i-1,3:x_iter) + (delta_y/2)*K(2,3:x_iter)) - 2*(X(i-1,2:x_iter-1) + (delta_y/2)*K(2,2:x_iter-1)) + (X(i-1,1:x_iter-2) + (delta_y/2)*K(2,1:x_iter-2)))/delta_x^2;
+        K(4,2:x_iter-1) = ((X(i-1,3:x_iter) + (delta_y)*K(3,3:x_iter)) - 2*(X(i-1,2:x_iter-1) + (delta_y)*K(3,2:x_iter-1)) + (X(i-1,1:x_iter-2) + (delta_y)*K(3,1:x_iter-2)))/delta_x^2;
+    
+        X(i,:) = X(i-1,:) + (delta_y/6)*(K(1,:) + K(2,:) + K(3,:) + K(4,:));
+        
+    end
+
+ str = "";
+    decide = input(str + "Press enter to exit. Type 1 for Finite Difference Solution", "s");
+    switch decide
+        case "1"
+
+            % Create meshgrid for surface plot
+            [X_grid, Y_grid] = meshgrid(x_vals, y_vals(1:80:end));  % Assuming x and y are 1D vectors
+            
+            figure;
+            hold on
+            h = surf(Y_grid, X_grid, X(1:80:end,:), "FaceAlpha", 0.25, 'FaceColor', 'b');
+            xlabel('x');
+            ylabel('y');
+            zlabel('Temperature');
+            title('2D Heat MOL Solution');
+            view(3); 
+        case isempty(decide)
+            return
+    end
+
+
+end
--- main.m
+++ main.m
@@ -1,7 +1,7 @@
 % These control the number of iterations in both space and time. Note that
 % this is not the number of steps, which would be either of these values
 % minus 1. The output matrices will be exactly these dimensions.
-t_iter_heat = 5000;
+y_iter_heat = 5000;
 x_iter_heat = 70;
 
 % The t-values used for analysis are contained in t_vals, and the x-values
@@ -12,7 +12,7 @@ x_iter_heat = 70;
 % and are the calculated values for RK4 and ode45 respectively. The step
 % sizes for both x and t are given by delta_x and delta_t respectively.
 disp("Basic Heat Equation:")
-[t_vals_heat, x_vals_heat, X_analytical_heat, X_heat, U_sol_heat, t_sol_heat, delta_x_heat, delta_t_heat] = basic_heat(t_iter_heat, x_iter_heat);
+[t_vals_heat, x_vals_heat, X_analytical_heat, X_heat, U_sol_heat, t_sol_heat, delta_x_heat, delta_t_heat] = basic_heat(y_iter_heat, x_iter_heat);
 
 % Wave Code
 t_iter_wave = 5000;
@@ -46,13 +46,10 @@ max_iter_pois = 40000;
 disp("Radiator Fin Poisson's Equation:")
 [x_vals_pois2, y_vals_pois2, X_pois2, delta_x_pois2, delta_y_pois2] = Radiator_fin(x_iter_pois, y_iter_pois, TOL_pois, max_iter_pois);
 
-%{
+%%
 % Radiator Heat Method of Lines
 
-x_iter_MOL = 101;
-y_iter_MOL = 201;
-TOL_MOL = 10^-8;
-max_iter_MOL = 20000;
+y_iter_heat = 6000;
+x_iter_heat = 40;
 disp("Radiator Fin MOL Equation:")
-[x_vals_MOL, y_vals_MOL, X_MOL, delta_x_MOL, delta_y_MOL] = Radiator_fin_MOL(x_iter_MOL, y_iter_MOL, TOL_MOL, max_iter_MOL);
-%}
+[t_vals_heat, x_vals_heat, X_heat, delta_x_heat, delta_t_heat] = Radiator_fin_MOL(y_iter_heat, x_iter_heat);
